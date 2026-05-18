f8d558a1bd4ed2362799d72e6aa1e2f69f2014dd
Victor Ramírez <108971947+VictorRamirez26@users.noreply.github.com>
2026-05-18 16:46:19 -0300

    Refactor serial control and semaphore handling

--- TP2/arduino/tp2.ino
+++ TP2/arduino/tp2.ino
@@ -28,106 +28,100 @@ void isrStop() {
 
 
 void TaskMain(void *pvParameters) {
-  while (1) {
 
-    // Control por serial (UI)
-    if (Serial.available()) {
-      char cmd = Serial.read();
+  // Control por serial (UI)
+  if (Serial.available()) {
+    char cmd = Serial.read();
 
-      if (cmd == '1') {
-        running = true;
-      } 
-      else if (cmd == '0') {
-        running = false;
-        alarm = false;
-      }
-    }
-
-    // Control por botones (interrupciones)
-    if (xSemaphoreTake(semStart, 0) == pdPASS) {
+    if (cmd == '1') {
       running = true;
-    }
-
-    if (xSemaphoreTake(semStop, 0) == pdPASS) {
+    } 
+    else if (cmd == '0') {
       running = false;
       alarm = false;
     }
+  }
 
-    // Lectura analógica
-    if (running) {
-      int value = analogRead(A3);
+  // Control por botones (interrupciones)
+  if (xSemaphoreTake(semStart, 0) == pdPASS) {
+    running = true;
+  }
+
+  if (xSemaphoreTake(semStop, 0) == pdPASS) {
+    running = false;
+    alarm = false;
+  }
+
+  // Lectura analógica
+  if (running) {
+    int value = analogRead(A3);
+
+    xSemaphoreTake(mutex, portMAX_DELAY);
+    lastValue = value;
+
+    if (value > 800) {
+      alarm = true;   // Queda activado
+    }
+
+    xSemaphoreGive(mutex);
+  }
+
+  // Serial output
+  if (running) {
+    static TickType_t lastPrint = 0;
+    if (xTaskGetTickCount() - lastPrint > pdMS_TO_TICKS(3000)) {
 
       xSemaphoreTake(mutex, portMAX_DELAY);
-      lastValue = value;
 
-      if (value > 800) {
-        alarm = true;   // Queda activado
-      }
+      Serial.print("L:");
+      Serial.print(lastValue);
+
+      Serial.print(",A:");
+      Serial.println(alarm ? 1 : 0);
 
       xSemaphoreGive(mutex);
+
+      lastPrint = xTaskGetTickCount();
     }
-
-    // Serial output
-    if (running) {
-      static TickType_t lastPrint = 0;
-      if (xTaskGetTickCount() - lastPrint > pdMS_TO_TICKS(3000)) {
-
-        xSemaphoreTake(mutex, portMAX_DELAY);
-
-        Serial.print("L:");
-        Serial.print(lastValue);
-
-        Serial.print(",A:");
-        Serial.println(alarm ? 1 : 0);
-
-        xSemaphoreGive(mutex);
-
-        lastPrint = xTaskGetTickCount();
-      }
-    }
-
-    vTaskDelay(50 / portTICK_PERIOD_MS);
   }
+
+  vTaskDelay(50 / portTICK_PERIOD_MS);
 }
 
 void TaskLeds(void *pvParameters) {
   pinMode(11, OUTPUT);
   pinMode(12, OUTPUT);
 
-  while (1) {
-    bool localAlarm;
+  bool localAlarm;
 
-    xSemaphoreTake(mutex, portMAX_DELAY);
-    localAlarm = alarm;
-    xSemaphoreGive(mutex);
+  xSemaphoreTake(mutex, portMAX_DELAY);
+  localAlarm = alarm;
+  xSemaphoreGive(mutex);
 
-    if (running) {
+  if (running) {
 
-      digitalWrite(11, HIGH);
-      vTaskDelay(500 / portTICK_PERIOD_MS);
-      digitalWrite(11, LOW);
-      vTaskDelay(500 / portTICK_PERIOD_MS);
+    digitalWrite(11, HIGH);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+    digitalWrite(11, LOW);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
 
-      if (localAlarm) {
-        digitalWrite(12, HIGH);
-        vTaskDelay(50 / portTICK_PERIOD_MS);
-        digitalWrite(12, LOW);
-        vTaskDelay(50 / portTICK_PERIOD_MS);
-      }
-
-    } else {
-      digitalWrite(11, LOW);
+    if (localAlarm) {
+      digitalWrite(12, HIGH);
+      vTaskDelay(50 / portTICK_PERIOD_MS);
       digitalWrite(12, LOW);
-      vTaskDelay(100 / portTICK_PERIOD_MS);
+      vTaskDelay(50 / portTICK_PERIOD_MS);
     }
+
+  } else {
+    digitalWrite(11, LOW);
+    digitalWrite(12, LOW);
+    vTaskDelay(100 / portTICK_PERIOD_MS);
   }
+  
 }
 
 void setup() {
   Serial.begin(9600);
-
-  pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
 
   pinMode(BTN_START, INPUT);
   pinMode(BTN_STOP, INPUT);
